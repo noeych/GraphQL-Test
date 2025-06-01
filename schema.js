1b708d672abfb2af1f171c0337eda15808451f43
noeych <codus1114@gmail.com>
2025-06-01 14:09:54 +0900

    CRUD 기본

---
+++ schema.js
@@ -0,0 +1,98 @@
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLSchema
+} = require('graphql');
+
+const User = require('./models/User');  // mongoose 모델
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        users: {
+        type: new GraphQLList(UserType),
+        resolve() {
+            return User.find();  // MongoDB에서 전체 사용자 조회
+        }
+        },
+        user: {
+        type: UserType,
+        args: { id: { type: GraphQLID } },
+        resolve(_, args) {
+            return User.findById(args.id);
+        }
+        }
+    }
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        // Create
+        createUser: {
+            type: UserType,
+            args: {
+                name: { type: GraphQLString },
+                email: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(_, args) {
+                const user = new User({
+                name: args.name,
+                email: args.email,
+                age: args.age
+                });
+                return user.save();
+            }
+        },
+        // Update
+        updateUser: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLID },
+                name: { type: GraphQLString },
+                email: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            async resolve(_, args) {
+                return await User.findByIdAndUpdate(
+                    args.id,
+                    {
+                        name: args.name,
+                        email: args.email,
+                        age: args.age
+                    },
+                    { new: true }  // 업데이트된 데이터 반환
+                );
+            }
+        },
+        // Delete
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: {type: GraphQLID}
+            },
+            async resolve(_, args) {
+                return await User.findByIdAndUpdate(args.id);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+});
--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`);
-
-const root = {
-    hello: () => 'Hello world!'
-};
+const mongoose = require('mongoose');
+const schema = require('./schema');
 
 const app = express();
+
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
-    graphiql: true,
+    graphiql: true
 }));
 
 app.listen(5110, () => {
     console.log('Running on http://localhost:5110/graphql');
 });
+
+mongoose.connect('mongodb+srv://user:userpassword@cluster.4ocney1.mongodb.net/database?retryWrites=true&w=majority&appName=Cluster')
+    .then(() => console.log('MongoDB 연결 완료'))
+    .catch(err => console.error('MongoDB 연결 실패:', err));
